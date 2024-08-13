83dfd4d21c94a2f64eac9756813d7cfdd4391b55
Precious <preciousonome002@gmail.com>
2024-08-13 15:03:05 +0100

    quagga

--- src/pages/homePage/ReachUsPage.jsx
+++ src/pages/homePage/ReachUsPage.jsx
@@ -8,9 +8,9 @@ const ReachUsPage = () => {
             <h2 className="text-gray-700 text-xl font-bold">Reach us</h2>
             <div className="text-gray-600 text-lg">
 
-                <a href="#" className="text-gray-400 mt-3 p-4"><FaLocationDot size="1.5em" /></a> 326 Herbert Macaulay Way, Lagos, Nigeria, ZIP 101212.
-               <br/> <a href="#" className="text-gray-400 mt-3 p-4"><FaPhone size="1.5em" /></a>+234 456 789 4572.
-               <br/> <a href="#" className="text-gray-400 mt-3 p-4"><FaMessage size="1.5em" /></a> info@quaggaapp.com
+                <a href="#" className="text-gray-400 mt-3 p-4"><FaLocationDot size="1.5em"/></a>326 Herbert Macaulay Way, Lagos, Nigeria, ZIP 101212.
+               <br/> <a href="#" className="text-gray-400 mt-3 p-4"><FaPhone size="1.5em"/></a>+234 456 789 4572.
+               <br/> <a href="#" className="text-gray-400 mt-3 p-4"><FaMessage size="1.5em"/></a> info@quaggaapp.com
             </div>
         </div>
     )

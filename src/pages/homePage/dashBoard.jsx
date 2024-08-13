cf200e65ed74f77fadd1617e5cecf720e759aeaf
Baliqis <sulaimabaliqis@gmail.com>
2024-08-13 17:18:28 +0100

    corrections

--- src/pages/homePage/dashBoard.jsx
+++ src/pages/homePage/dashBoard.jsx
@@ -71,6 +71,7 @@ const DashBoard = () => {
             <div className='flex'>
                 <Sidebar/>
                 <div>
+                    {/*fff*/}
                     <section className="container mx-auto p-10 md:py-12 px-0 md:p-8 md:px-0">
                         <section
                             className="p-5 md:p-0 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 items-start ">

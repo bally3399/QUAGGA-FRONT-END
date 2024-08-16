ccbfa049fd473a01f677db06d3828eeeaba3895f
josephEmeka <josephemekaonah@gmail.com>
2024-08-16 10:55:36 +0100

    price edit

--- src/pages/homePage/Heropage.jsx
+++ src/pages/homePage/Heropage.jsx
@@ -25,10 +25,8 @@ const HeroPage = () => {
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
                         <h2 className="max-w-lg mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:mb-6 group">
-                            <span className="inline-block mb-1 sm:mb-4">
+                            <span className=" justify-center inline-block mb-1 sm:mb-4">
                                 Quality you can trust
-                                <br className="hidden md:block"/>
-                               ...at your fingertips
                             </span>
                             <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100"/>
                         </h2>

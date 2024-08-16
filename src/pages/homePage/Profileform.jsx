19bcaf8a73f5a5c687a514bcd97c64094e76abf5
josephEmeka <josephemekaonah@gmail.com>
2024-08-16 10:29:50 +0100

    profile form edit

--- src/pages/homePage/Profileform.jsx
+++ src/pages/homePage/Profileform.jsx
@@ -55,7 +55,7 @@ const ProfileForm = ()=> {
                                                     className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">quagga.com/</span>
                                                 <input type="text" name="username" id="username" autoComplete="username"
                                                        className="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
-                                                       defaultValue="deblewis"/>
+                                                       defaultValue="email"/>
                                             </div>
                                         </div>
 
@@ -63,7 +63,7 @@ const ProfileForm = ()=> {
                                             <label htmlFor="about"
                                                    className="block text-sm font-medium text-gray-700">About</label>
                                             <div className="mt-1">
-                                                <textarea id="about" name="about" rows="3"
+                                                <textarea id="about" name="about" rows="6"
                                                           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"></textarea>
                                             </div>
                                             <p className="mt-2 text-sm text-gray-500">Brief description for your
@@ -134,16 +134,10 @@ const ProfileForm = ()=> {
                                                    className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"/>
                                         </div>
 
-                                        <div className="col-span-12">
-                                            <label htmlFor="url"
-                                                   className="block text-sm font-medium text-gray-700">URL</label>
-                                            <input type="text" name="url" id="url"
-                                                   className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"/>
-                                        </div>
 
                                         <div className="col-span-12 sm:col-span-6">
                                             <label htmlFor="company"
-                                                   className="block text-sm font-medium text-gray-700">Company</label>
+                                                   className="block text-sm font-medium text-gray-700">Company name</label>
                                             <input type="text" name="company" id="company" autoComplete="organization"
                                                    className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"/>
                                         </div>

9ded3308088d92ecb355edc5bc3eecc98a922cdc
Baliqis <sulaimabaliqis@gmail.com>
2024-08-16 11:05:41 +0100

    corrections on profile, heropage

--- src/pages/homePage/profile.jsx
+++ src/pages/homePage/profile.jsx
@@ -2,7 +2,7 @@
 import Navbar from "../../component/navbar/Navbar";
 import React from "react";
 
-const Profile =()=>{
+const Profile = () => {
     return (
         <>
             <Navbar />
@@ -14,18 +14,18 @@ const Profile =()=>{
                                 <div className="bg-white shadow rounded-lg p-6">
                                     <div className="flex flex-col items-center">
                                         <div
-                                             className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
+                                            className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
                                         </div>
                                         <h1 className="text-xl font-bold">John Doe</h1>
                                         <p className="text-gray-700">Software Developer</p>
                                         <div className="mt-6 flex flex-wrap gap-4 justify-center">
                                             <a href="#"
-                                               className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Contact</a>
+                                                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Contact</a>
                                             <a href="/profile-form"
-                                               className="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded">Edit Profile</a>
+                                                className="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded">Edit Profile</a>
                                         </div>
                                     </div>
-                                    <hr className="my-6 border-t border-gray-300"/>
+                                    <hr className="my-6 border-t border-gray-300" />
                                     <div className="flex flex-col">
                                         <span
                                             className="text-gray-700 uppercase font-bold tracking-wider mb-2">Catagory</span>
@@ -46,8 +46,8 @@ const Profile =()=>{
                                     <h2 className="text-xl font-bold mb-4">About Me</h2>
                                     <label htmlFor="about"></label>
                                     <div className="mt-1">
-                                                <textarea id="about" name="about" rows="3"
-                                                          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"></textarea>
+                                        <textarea id="about" name="about" rows="3"
+                                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"></textarea>
                                     </div>
                                     <p className="mt-2 text-sm text-gray-500">Tell us about yourself</p>
 
@@ -55,18 +55,18 @@ const Profile =()=>{
                                     <div className="mb-6">
                                         <label htmlFor="experience"></label>
                                         <div className="mt-1">
-                                                <textarea id="about" name="about" rows="3"
-                                                          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"></textarea>
+                                            <textarea id="about" name="about" rows="3"
+                                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm"></textarea>
                                         </div>
                                         <p className="mt-2 text-sm text-gray-500">Tell us about your experience</p>
                                     </div>
 
                                 </div>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
         </>
     )

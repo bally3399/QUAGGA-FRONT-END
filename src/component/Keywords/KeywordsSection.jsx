18c47742c798c642dd9c007216405b573ea01559
josephEmeka <josephemekaonah@gmail.com>
2024-09-11 12:59:08 +0100

    more edits

--- src/component/Keywords/KeywordsSection.jsx
+++ src/component/Keywords/KeywordsSection.jsx
@@ -89,7 +89,7 @@ const KeywordsSection = () => {
                     {displayedKeywords.map((keyword, index) => (
                         <li key={index}>
                             <button
-                                className="text-gray-500 hover:text-gray-700 border border-gray-300 rounded px-3 py-1 text-sm"
+                                className="text-white hover:text-gray-700 hover:bg-sky-200  text-sm rounded-full border border-sky-100 bg-sky-50 px-2 py-0.5 dark:text-sky-900 dark:border-sky-500/15 dark:bg-sky-500/10 ..."
                                 onClick={() => handleKeywordClick(keyword)}
                             >
                                 {keyword}
@@ -100,15 +100,14 @@ const KeywordsSection = () => {
                 {!showAll && (
                     <div className="mt-4">
                         <button
-                            className="text-gray-500 hover:text-gray-700 border border-gray-300 rounded px-3 py-1 text-sm"
-                            onClick={() => setShowAll(true)}
+                            className="text-white hover:text-gray-700 hover:bg-sky-200  text-sm rounded-full border border-sky-100 bg-sky-50 px-2 py-0.5 dark:text-sky-900 dark:border-sky-500/15 dark:bg-sky-500/10 ..."                            onClick={() => setShowAll(true)}
                         >
                             See all
                         </button>
                     </div>
                 )}
 
-                {loading && <p>Loading...</p>}
+                {showAll && loading && <p>Loading...</p>}
 
                 <div className="mt-8">
                     {data.products.length > 0 && (
